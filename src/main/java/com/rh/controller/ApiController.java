db7c2d434be1afb3335fd0d737a053c438f5f164
limhaw <lim.swee.haw@revenue.com.my>
2019-03-03 12:12:27 +0800

    create sample API

---
+++ src/main/java/com/rh/controller/ApiController.java
@@ -0,0 +1,32 @@
+package com.rh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sweehaw
+ */
+@RestController
+public class ApiController {
+
+    @GetMapping("user")
+    public List<String> listAllUser() {
+        List<String> userList = new ArrayList<>();
+        userList.add("user1");
+        userList.add("user2");
+        userList.add("user3");
+        userList.add("user4");
+        userList.add("user5");
+
+        return userList;
+    }
+
+    @GetMapping("user/{user}")
+    public String listAllUser(@PathVariable String user) {
+        return user;
+    }
+}
---
+++ src/main/resources/webjars/js/my.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    $("#listUser").click(function() {
+        var ajaxSetting = {
+            url: "http://localhost:8080/user",
+            data: {},
+            type: "GET",
+            success: function(response) {
+                alert(response);
+            }
+        };
+        $.ajax(ajaxSetting);
+    });
+
+    $("#getUser").click(function() {
+        var ajaxSetting = {
+            url: "http://localhost:8080/user/user1",
+            data: {},
+            type: "GET",
+            success: function(response) {
+                alert(response);
+            }
+        };
+        $.ajax(ajaxSetting);
+    });
+});
